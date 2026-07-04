1d71ce7265d7de12590485b9edc42a8bb2555561
deryaunluoz <deryaunluoz@icloud.com>
2026-07-04 15:26:18 +0400

    CORS Vercel domain eklendi

--- src/main/java/com/oceantech/backend/config/SecurityConfig.java
+++ src/main/java/com/oceantech/backend/config/SecurityConfig.java
@@ -32,7 +32,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:5174",
+                "https://ocean-tech-xi.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
